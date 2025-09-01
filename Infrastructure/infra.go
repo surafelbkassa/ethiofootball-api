cbbb51bbdd06a3716fd6455f4af94865dc66dd38
Geda <gediizeyuu@gmail.com>
2025-09-01 15:25:53 +0300

    connect app to redis cloud

---
+++ Infrastructure/infra.go
@@ -0,0 +1,33 @@
+package infrastrucutre
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/redis/go-redis/v9"
+)
+
+func RedisConnect() *redis.Client {
+	ctx := context.Background()
+	redisAddress  := os.Getenv("REDIS_ADDRESS")
+	redisUsername := os.Getenv("REDIS_USERNAME")
+	redisPassword := os.Getenv("REDIS_PASSWORD")
+	
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     redisAddress,
+		Username: redisUsername,
+		Password: redisPassword,
+		DB:       0,
+	})
+
+	rdb.Set(ctx, "foo", "bar", 0)
+	result, err := rdb.Get(ctx, "foo").Result()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(result) 
+	return rdb
+}
