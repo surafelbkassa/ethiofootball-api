8681700fea170c09f8c66b48c20002b84753114a
Abel Tesfa <abeltesfa198@gmail.com>
2025-09-02 14:53:15 +0300

    Past Events News Integrated

--- Repository/event_repository.go
+++ Repository/event_repository.go
@@ -9,35 +9,35 @@ import (
 	domain "github.com/abrshodin/ethio-fb-backend/Domain"
 )
 
-type EventRepository struct {
-    apiURL string
+type EventRepositoryImpl struct {
+	apiURL string
 }
 
-func NewEventRepository() *EventRepository {
-    return &EventRepository{
-        apiURL: "https://www.thesportsdb.com/api/v1/json/123/eventspastleague.php?id=4959",
-    }
+func NewEventRepository() *EventRepositoryImpl {
+	return &EventRepositoryImpl{
+		apiURL: "https://www.thesportsdb.com/api/v1/json/123/eventspastleague.php?id=4959",
+	}
 }
 
-func (r *EventRepository) GetPastEvents() ([]domain.Event, error) {
-    resp, err := http.Get(r.apiURL)
-    if err != nil {
-        return nil, fmt.Errorf("failed to fetch events: %v", err)
-    }
-    defer resp.Body.Close()
+func (r *EventRepositoryImpl) GetPastEvents() ([]domain.Event, error) {
+	resp, err := http.Get(r.apiURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch events: %v", err)
+	}
+	defer resp.Body.Close()
 
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        return nil, fmt.Errorf("failed to read response body: %v", err)
-    }
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read response body: %v", err)
+	}
 
-    var result struct {
-        Events []domain.Event `json:"events"`
-    }
+	var result struct {
+		Events []domain.Event `json:"events"`
+	}
 
-    if err := json.Unmarshal(body, &result); err != nil {
-        return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
-    }
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal JSON: %v", err)
+	}
 
-    return result.Events, nil
+	return result.Events, nil
 }
--- Usecase/news_usecase.go
+++ Usecase/news_usecase.go
@@ -1,18 +1,22 @@
-// usecases/news_usecase.go
 package usecase
 
 import (
 	"fmt"
+	"strconv"
 	"time"
 
-	repository "github.com/abrshodin/ethio-fb-backend/Repository"
+	domain "github.com/abrshodin/ethio-fb-backend/Domain"
 )
 
-type NewsUseCase struct {
-	repo *repository.EventRepository
+type EventRepository interface {
+	GetPastEvents() ([]domain.Event, error)
 }
 
-func NewNewsUseCase(repo *repository.EventRepository) *NewsUseCase {
+type NewsUseCase struct {
+	repo EventRepository
+}
+
+func NewNewsUseCase(repo EventRepository) *NewsUseCase {
 	return &NewsUseCase{repo: repo}
 }
 
@@ -24,26 +28,26 @@ func (uc *NewsUseCase) GenerateNews() ([]string, error) {
 
 	var news []string
 	for _, e := range events {
-		// Parse and format date nicely
 		dateStr := e.DateEvent
 		if parsedDate, err := time.Parse("2006-01-02", e.DateEvent); err == nil {
 			dateStr = parsedDate.Format("02 Jan 2006")
 		}
 
-		// Create a more descriptive headline
+		// Convert string scores to int
+		homeScore, _ := strconv.Atoi(e.IntHomeScore)
+		awayScore, _ := strconv.Atoi(e.IntAwayScore)
+
 		var resultDesc string
-		if e.IntHomeScore == e.IntAwayScore {
-			resultDesc = fmt.Sprintf("%s and %s played to a %d-%d draw", e.StrHomeTeam, e.StrAwayTeam, e.IntHomeScore, e.IntAwayScore)
-		} else if e.IntHomeScore > e.IntAwayScore {
-			resultDesc = fmt.Sprintf("%s edged out %s with a %d-%d victory", e.StrHomeTeam, e.StrAwayTeam, e.IntHomeScore, e.IntAwayScore)
-		} else {
-			resultDesc = fmt.Sprintf("%s dominated %s with a %d-%d win", e.StrAwayTeam, e.StrHomeTeam, e.IntAwayScore, e.IntHomeScore)
+		switch {
+		case homeScore == awayScore:
+			resultDesc = fmt.Sprintf("%s and %s played to a %d-%d draw", e.StrHomeTeam, e.StrAwayTeam, homeScore, awayScore)
+		case homeScore > awayScore:
+			resultDesc = fmt.Sprintf("%s edged out %s with a %d-%d victory", e.StrHomeTeam, e.StrAwayTeam, homeScore, awayScore)
+		default:
+			resultDesc = fmt.Sprintf("%s dominated %s with a %d-%d win", e.StrAwayTeam, e.StrHomeTeam, awayScore, homeScore)
 		}
 
-		headline := fmt.Sprintf(
-			"%s on %s | Status: %s",
-			resultDesc, dateStr, e.StrStatus,
-		)
+		headline := fmt.Sprintf("%s on %s | Status: %s", resultDesc, dateStr, e.StrStatus)
 		news = append(news, headline)
 	}
 
